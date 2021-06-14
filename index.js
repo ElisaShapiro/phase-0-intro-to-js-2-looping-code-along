a7b2c6884fd8953c777b651d42106794bb832bf9
Elisa Shapiro <elisashapiro@gmail.com>
2021-06-14 13:14:36 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+const names = ['Lisa', 'Kaitlin', 'Jan'];
+var event = 'surprise'
+
+function writeCards(names, event) {
+    let final = [];
+    for (let i = 0; i < names.length; i++){
+        final.push (`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+return final;
+}
+let finishedCards = writeCards(['Lisa', 'Kaitlin', 'Jan'], 'birthday');
+console.log (finishedCards);
+
+function countDown() {
+    for (let countDown = 10; countDown >= 0; countDown--) {
+        console.log(countDown);
+    }
+}
